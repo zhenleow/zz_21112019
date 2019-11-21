02cd59b7b4e087a8fae6df20ae7eddf71bc23a01
zhenleow <leowzhen@gmail.com>
2019-11-21 11:00:18 +0800

    Update application.py

--- helloworld/application.py
+++ helloworld/application.py
@@ -11,7 +11,7 @@ def get():
 
 @application.route('/', methods=['POST'])
 def post():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    return Response(json.dumps({'Output': 'Hello World from Code Star_Team2'}), mimetype='application/json', status=200)
 
 if __name__ == '__main__':
     flaskrun(application)
